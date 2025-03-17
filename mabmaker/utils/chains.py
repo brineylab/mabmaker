f96c845d1faf7be543c7ba37fe2692ce2d203a8e
Bryan Briney <bryanbriney@gmail.com>
2025-03-17 10:26:18 -0700

    add boltz alphabet generator (identical to the Chai alphabet generator)

--- mabmaker/utils/chains.py
+++ mabmaker/utils/chains.py
@@ -14,6 +14,8 @@ def get_chain_name_generator(format: str = "chai"):
         return chai_alphabet_generator()
     elif format == "protenix":
         return protenix_alphabet_generator()
+    elif format == "boltz":
+        return boltz_alphabet_generator()
     else:
         raise ValueError(f"Invalid format: {format}")
 
@@ -67,6 +69,51 @@ def chai_alphabet_generator():
             for chars in itertools.product(letters, repeat=N):
                 yield "".join(chars)
         N += 1
+
+
+def boltz_alphabet_generator():
+    """
+    Generate an alphabet of increasing length using only uppercase and lowercase letters.
+    This is identical to the `new naming scheme for chains in Chai`_, which we're repurposaing
+    for use with Boltz, which has a `maximum chain length of 5 characters`_.
+
+    .. _new naming scheme for chains in Chai: https://github.com/chaidiscovery/chai-lab/issues/290
+    .. _maximum chain length of 5 characters: https://github.com/jwohlwend/boltz/issues/181
+
+    Examples
+    --------
+    >>> alphabet_generator()
+    'A'
+    >>> next(alphabet_generator())
+    'B'
+     .
+     .
+     .
+    >>> next(alphabet_generator())
+    'a'
+    >>> next(alphabet_generator())
+    'b'
+     .
+     .
+     .
+    >>> next(alphabet_generator())
+    'AB'
+    >>> next(alphabet_generator())
+    'AC'
+     .
+     .
+     .
+    >>> next(alphabet_generator())
+    'aa'
+    >>> next(alphabet_generator())
+    'ab'
+
+    Yields
+    ------
+    str
+        The next chain name in the sequence.
+    """
+    return chai_alphabet_generator()
 
 
 def protenix_alphabet_generator():
