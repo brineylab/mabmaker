b5e728afb4374195bde20e868c2994873f8603b4
Bryan Briney <bryanbriney@gmail.com>
2025-03-17 22:08:07 -0700

    gpu_worker can now also accept a Callable (not just a string to be called using subprocess.run)

--- mabmaker/utils/jobs.py
+++ mabmaker/utils/jobs.py
@@ -5,13 +5,13 @@
 
 import queue
 import subprocess as sp
-from typing import Iterable
+from typing import Callable, Iterable
 
 import torch
 
 
 def gpu_worker(
-    cmd: str,
+    cmd: str | Callable,
     gpu_queue: queue.Queue,
 ) -> None:
     """
@@ -20,8 +20,10 @@ def gpu_worker(
     Parameters
     ----------
     cmd : str
-        The command to run, as a string. Should not include the ``CUDA_VISIBLE_DEVICES``
-        environment variable, as this will be added by the `gpu_worker` function.
+        The command to run, either as a string (which will be called using ``subprocess.run()``) or as a callable function.
+        If a string is supplied, it should not include the ``CUDA_VISIBLE_DEVICES`` environment variable, as this will
+        be added by the `gpu_worker` function. If a callable is supplied, it should take a single argument: ``device``,
+        which accepts the GPU ID formatted as a string (e.g. ``"cuda:0"``).
 
     gpu_queue : queue.Queue
         A queue of GPU IDs which will be used to determine which GPU to use for the command.
@@ -34,8 +36,11 @@ def gpu_worker(
     """
     gpu_id = gpu_queue.get()
     try:
-        cmd = f"CUDA_VISIBLE_DEVICES={gpu_id} {cmd}"
-        result = sp.run(cmd, shell=True, stdout=sp.PIPE, stderr=sp.PIPE)
+        if isinstance(cmd, str):
+            cmd = f"CUDA_VISIBLE_DEVICES={gpu_id} {cmd}"
+            result = sp.run(cmd, shell=True, stdout=sp.PIPE, stderr=sp.PIPE)
+        else:
+            result = cmd(device=f"cuda:{gpu_id}")
     finally:
         gpu_queue.put(gpu_id)
     return result
@@ -76,7 +81,7 @@ def get_gpu_queue(gpus: int | Iterable[int | str] | str | None = None) -> queue.
     if not gpus:
         raise ValueError("No GPUs specified.")
 
-    # create queue
+    # create the GPU queue
     gpu_queue = queue.Queue()
     for gpu in gpus:
         gpu_queue.put(gpu)
