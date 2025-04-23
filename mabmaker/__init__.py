096316ce6ff8f3434b632aca78e6a586098b76a5
Bryan Briney <bryanbriney@gmail.com>
2025-04-23 15:54:19 -0400

    Update __init__.py

--- mabmaker/__init__.py
+++ mabmaker/__init__.py
@@ -1,1 +1,10 @@
+# Copyright (c) 2025 brineylab @ scripps
+# Distributed under the terms of the MIT License.
+# SPDX-License-Identifier: MIT
+
+
+import warnings
+
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+
 from . import tl, tools, utils
