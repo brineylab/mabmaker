5b9499e7d984b01c6c9c76180379556b91b286c7
Bryan Briney <bryanbriney@gmail.com>
2025-04-29 08:43:26 -0700

    Update protenix.py

--- mabmaker/tools/protenix.py
+++ mabmaker/tools/protenix.py
@@ -11,7 +11,7 @@ import abutils
 from tqdm.auto import tqdm
 
 from ..utils.inputs import StructurePredictionRun, setup_structure_prediction_run
-from ..utils.jobs import get_gpu_queue, gpu_worker, silence_worker
+from ..utils.jobs import get_gpu_queue, gpu_worker
 from ..utils.outputs import process_protenix_output
 
 PROTENIX_DIR = os.path.abspath(
@@ -77,9 +77,7 @@ def protenix(
     # run predictions
     futures = []
     output_paths = []
-    with cf.ThreadPoolExecutor(
-        max_workers=num_gpus, initializer=silence_worker
-    ) as executor:
+    with cf.ThreadPoolExecutor(max_workers=num_gpus) as executor:
         for run in runs:
             run_output_path = os.path.join(output_path, run.name, "raw_output")
             cmd = _build_protenix_command(
